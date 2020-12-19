513c148669881e75b1867e327a184626351af9ec
ShivaPrasad R <64139813+shivaprasadrpllc@users.noreply.github.com>
2020-12-19 12:58:49 +0530

    App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SHOPALL This is For Testing';
+
+  constructor() {
+    console.log('Constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('New Log');
+  }
 }
