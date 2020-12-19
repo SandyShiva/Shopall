350d433af86f4b0a0306348d961e6e6cfc6b8375
ShivaPrasad R <64139813+shivaprasadrpllc@users.noreply.github.com>
2020-12-19 14:53:33 +0530

    Back to normal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+<<<<<<< HEAD
+export class AppComponent implements OnInit {
   title = 'SHOPALL This is For Testing';
+
+  constructor() {
+    console.log('Constructor');
+  }
+
+  ngOnInit(): void {
+    console.log('New Log');
+  }
+=======
+export class AppComponent {
+  title = 'SHOPALL';
+>>>>>>> parent of fb6a880 (Changes)
 }
