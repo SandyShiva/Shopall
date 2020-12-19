f42f790d5c20007742e4a086cb5523bc92584320
ShivaPrasad R <64139813+shivaprasadrpllc@users.noreply.github.com>
2020-12-19 14:55:20 +0530

    New changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-<<<<<<< HEAD
 export class AppComponent implements OnInit {
   title = 'SHOPALL This is For Testing';
 
@@ -16,8 +15,4 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     console.log('New Log');
   }
-=======
-export class AppComponent {
-  title = 'SHOPALL';
->>>>>>> parent of fb6a880 (Changes)
 }
